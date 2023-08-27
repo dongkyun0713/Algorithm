3e3a087787ff575a17f7d9a6f889a48af70f25d0
dongkyun <kyun010713@gmail.com>
2023-08-27 23:43:50 +0900

    B2903

---
+++ src/level_8/B2903.java
@@ -0,0 +1,14 @@
+package level_8;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B2903 {  // 중앙 이동 알고리즘 https://www.acmicpc.net/problem/2903
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int answer = (int) Math.pow(Math.pow(2,N)+1,2);
+        System.out.println(answer);
+    }
+}
