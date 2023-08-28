c2b7a7134a254a7317bb1dd7d1d1cf7a055cf907
dongkyun <kyun010713@gmail.com>
2023-08-28 23:58:08 +0900

    B5086

---
+++ src/level_9/B5086.java
@@ -0,0 +1,22 @@
+package level_9;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B5086 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while(true) {
+            String[] input = br.readLine().split(" ");
+            int a = Integer.parseInt(input[0]);
+            int b = Integer.parseInt(input[1]);
+            if(a == 0 && b == 0) {
+                break;
+            }
+            if(b % a == 0) System.out.println("factor");
+            else if(a % b == 0) System.out.println("multiple");
+            else System.out.println("neither");
+        }
+    }
+}
