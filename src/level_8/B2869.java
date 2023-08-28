62a0c90ae8a0c8ae2d7ab9c633f9ffd44e5bfb9b
dongkyun <kyun010713@gmail.com>
2023-08-28 20:59:52 +0900

    B2869

---
+++ src/level_8/B2869.java
@@ -0,0 +1,18 @@
+package level_8;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B2869 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int A = Integer.parseInt(input[0]);
+        int B = Integer.parseInt(input[1]);
+        int V = Integer.parseInt(input[2]);
+        int day = (V-B) / (A-B);
+        if((V-B) % (A-B) != 0) day++;
+        System.out.println(day);
+    }
+}
