bf1519379f15255a8ffbc4ee38f06b692852f692
dongkyun <kyun010713@gmail.com>
2023-09-03 19:03:16 +0900

    main

---
+++ src/bronze_4/B4263_알고리즘의수행시간2.java
@@ -0,0 +1,14 @@
+package bronze_4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B4263_알고리즘의수행시간2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String n = br.readLine();
+        System.out.println(n);
+        System.out.println(1);
+    }
+}
