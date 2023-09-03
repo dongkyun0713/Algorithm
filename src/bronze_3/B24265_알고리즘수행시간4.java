1e2cbe2be28a370b9a44d3dc6d5b43baed77b418
dongkyun <kyun010713@gmail.com>
2023-09-03 21:26:27 +0900

    main

---
+++ src/bronze_3/B24265_알고리즘수행시간4.java
@@ -0,0 +1,10 @@
+package bronze_3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class B24265_알고리즘수행시간4 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
---
+++ src/silver_3/B1449_수리공항승.java
@@ -0,0 +1,37 @@
+package silver_3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class B1449_수리공항승 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input[] = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int L = Integer.parseInt(input[1]);
+
+        String input2[] = br.readLine().split(" ");
+        int arr[] = new int[N];
+        if(L == 1) System.out.println(N);
+        else {
+            for (int i = 0; i < N; i++) {
+                arr[i] = Integer.parseInt(input2[i]);
+            }
+            Arrays.sort(arr);
+            double tape = arr[0] - 0.5;
+            int count = 1;
+            for (int i = 0; i < N; i++) {
+                if(tape + L < arr[i]) {
+                    count++;
+                    tape = arr[i] - 0.5;
+                }
+            }
+            System.out.println(count);
+        }
+    }
+}
+//4 2
+//1 2 100 101
