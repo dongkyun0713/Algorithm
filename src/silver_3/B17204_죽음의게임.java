13c9547cf5d746a7bbdce8ba53e109665162bed1
dongkyun <kyun010713@gmail.com>
2023-09-04 23:32:28 +0900

    origin

---
+++ src/silver_3/B17204_죽음의게임.java
@@ -0,0 +1,17 @@
+package silver_3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B17204_죽음의게임 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input[] = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int K = Integer.parseInt(input[1]);
+
+
+    }
+}
