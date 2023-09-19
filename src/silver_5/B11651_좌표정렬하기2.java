6169174d969f4eda42c7c6ae70a2d1a2c6335c65
dongkyun <kyun010713@gmail.com>
2023-09-19 19:51:03 +0900

    main

---
+++ src/silver_5/B11651_좌표정렬하기2.java
@@ -0,0 +1,31 @@
+package silver_5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+
+public class B11651_좌표정렬하기2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int arr[][] = new int[n][2];
+
+        for(int i = 0; i < n; i++) {
+            String input[] = br.readLine().split(" ");
+            arr[i][0] = Integer.parseInt(input[0]);
+            arr[i][1] = Integer.parseInt(input[1]);
+        }
+
+        Arrays.sort(arr, (o1, o2) -> {
+            return o1[1] != o2[1] ? o1[1] - o2[1] : o1[0] - o2[0];
+
+        });
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n; i++) {
+            sb.append(arr[i][0]).append(" ").append(arr[i][1]).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
