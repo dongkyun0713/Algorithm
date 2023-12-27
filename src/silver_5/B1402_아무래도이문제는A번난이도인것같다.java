c78763f00500db570aabe779417617291313a543
dongkyun <kyun010713@gmail.com>
2023-12-27 13:28:50 +0900

    B1402_아무래도이문제는A번난이도인것같다

---
+++ src/silver_5/B1402_아무래도이문제는A번난이도인것같다.java
@@ -0,0 +1,20 @@
+package silver_5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B1402_아무래도이문제는A번난이도인것같다 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+        String[] input = br.readLine().split(" ");
+
+        for (int i = 0; i < t; i++) {
+            int a = Integer.parseInt(input[0]);
+            int b = Integer.parseInt(input[1]);
+            System.out.println("yes");
+        }
+    }
+}
