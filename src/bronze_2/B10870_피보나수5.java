60a0677df19a1a9f5a946245b5496d7abccab709
dongkyunKim <kyun010713@gmail.com>
2024-04-17 23:40:40 +0900

    [B5] : B10870

---
+++ src/bronze_2/B10870_피보나수5.java
@@ -0,0 +1,12 @@
+package bronze_2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B10870_피보나수5 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
