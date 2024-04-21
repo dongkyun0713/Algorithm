3525b3972e6c5360613e2d210331156e906429dc
dongkyunKim <kyun010713@gmail.com>
2024-04-21 22:58:46 +0900

    [B5] : B5622

---
+++ src/bronze_2/B5622.java
@@ -0,0 +1,32 @@
+package bronze_2;
+
+import java.io.*;
+public class B5622 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        String s = br.readLine();
+        int n = 0;
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) >= 'A' && s.charAt(i) <= 'C')
+                n += 3;
+            else if(s.charAt(i) >= 'D' && s.charAt(i) <= 'F')
+                n += 4;
+            else if(s.charAt(i) >= 'G' && s.charAt(i) <= 'I')
+                n += 5;
+            else if(s.charAt(i) >= 'J' && s.charAt(i) <= 'L')
+                n += 6;
+            else if(s.charAt(i) >= 'M' && s.charAt(i) <= 'O')
+                n += 7;
+            else if(s.charAt(i) >= 'P' && s.charAt(i) <= 'S')
+                n += 8;
+            else if(s.charAt(i) >= 'T' && s.charAt(i) <= 'V')
+                n += 9;
+            else
+                n+= 10;
+        }
+        System.out.println(n);
+    }
+}
