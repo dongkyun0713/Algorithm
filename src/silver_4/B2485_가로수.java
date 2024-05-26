ba86211c1625119c1ca4f53af191b84d21223435
dongkyunKim <kyun010713@gmail.com>
2024-05-27 00:12:46 +0900

    fix:B2465

--- src/silver_4/B2485_가로수.java
+++ src/silver_4/B2485_가로수.java
@@ -3,7 +3,6 @@ package silver_4;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class B2485_가로수 {
     public static void main(String[] args) throws IOException {
@@ -18,13 +17,18 @@ public class B2485_가로수 {
 
         int intervalGcd = 0;
 
-        for (int i = 0; i < n-1; i++) {
-            int distance = arr[i+1] - arr[i];
+        for (int i = 0; i < n - 1; i++) {
+            int distance = arr[i + 1] - arr[i];
             intervalGcd = gcd(distance, intervalGcd);
         }
+
+        System.out.println((arr[n - 1] - arr[0]) / intervalGcd - n + 1);
     }
+
     private static int gcd(int a, int b) {
-        if (b == 0) return a;
+        if (b == 0) {
+            return a;
+        }
         return gcd(b, a % b);
     }
 }
