029c163d48300a09ff2a94a855649aa54491a9a4
dongkyunKim <kyun010713@gmail.com>
2024-06-02 23:46:29 +0900

    feat:B:18258

---
+++ src/silver_4/B18258_큐2.java
@@ -0,0 +1,16 @@
+package silver_4;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class B18258_큐2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n
+    }
+}
