c655a4f34f0d9c39ea43527e58782f811d18de63
dongkyunKim <kyun010713@gmail.com>
2024-07-02 23:45:46 +0900

    feat: P42840

---
+++ src/programmers/level_1/p42840.java
@@ -0,0 +1,18 @@
+package level_1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class p42840 {
+    public int[] solution(int[] answers) {
+        int[] answer = {};
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        List<Integer> list3 = new ArrayList<>();
+
+        for (int i = 0; i < answers.length; i++) {
+
+        }
+        return answer;
+    }
+}
