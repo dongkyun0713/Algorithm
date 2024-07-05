c631e0fa21cff6b9dc4ca85d3910759162916834
dongkyunKim <kyun010713@gmail.com>
2024-07-05 23:46:18 +0900

    feat(baekjoon): B15649_N과M

---
+++ src/baekjoon/silver_3/B15649_N과M.java
@@ -0,0 +1,16 @@
+package baekjoon.silver_3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B15649_N과M {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        int n = Integer.parseInt(s[0]);
+        int m = Integer.parseInt(s[1]);
+
+
+    }
+}
