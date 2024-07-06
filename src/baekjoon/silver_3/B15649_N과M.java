ea6df3323bdfaedd185c4031bb37e77e47f5e566
dongkyunKim <kyun010713@gmail.com>
2024-07-06 23:30:47 +0900

    fix(baekjoon): B15649_N과M

--- src/baekjoon/silver_3/B15649_N과M.java
+++ src/baekjoon/silver_3/B15649_N과M.java
@@ -5,12 +5,40 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class B15649_N과M {
+    static int n;
+    static int m;
+    static int[] arr;
+    static boolean[] visit;
+    static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] s = br.readLine().split(" ");
-        int n = Integer.parseInt(s[0]);
-        int m = Integer.parseInt(s[1]);
+        String[] input = br.readLine().split(" ");
+        n = Integer.parseInt(input[0]);
+        m = Integer.parseInt(input[1]);
 
+        arr = new int[m];
+        visit = new boolean[n];
 
+        dfs(0);
+        System.out.print(sb);
+    }
+
+    public static void dfs(int depth) {
+        if (depth == m) {
+            for(int k : arr) {
+                sb.append(k).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                arr[depth] = i + 1;
+                dfs(depth + 1);
+                visit[i] = false;
+            }
+        }
     }
 }
