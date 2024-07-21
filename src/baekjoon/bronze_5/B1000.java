77613a4582c7c0fde0a42afebc337afcaf4b45d7
dongkyunKim <kyun010713@gmail.com>
2024-07-21 14:44:08 +0900

    feat(Baekjoon): 입력 성능 개선

--- src/baekjoon/bronze_5/B1000.java
+++ src/baekjoon/bronze_5/B1000.java
@@ -1,15 +1,14 @@
 package baekjoon.bronze_5;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class B1000 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-
-        int a = scanner.nextInt();
-
-        int b = scanner.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
 
         System.out.print(a + b);
     }
