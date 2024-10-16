dbdf822bec3fca44b9291ce390866a2ade5ddf2c
dongkyunKim <kyun010713@gmail.com>
2024-10-16 21:43:41 +0900

    feat(Programmers): P49189

---
+++ src/programmers/level_3/P49189.java
@@ -0,0 +1,13 @@
+package level_3;
+
+import java.util.Queue;
+
+public class P49189 {
+    public int solution(int n, int[][] edge) {
+        int answer = 0;
+        return answer;
+    }
+    public void bfs(int i) {
+
+    }
+}
