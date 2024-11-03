3cdf8e5dc9840cf4afe3718afa2e77f2e7f16eaa
dongkyunKim <kyun010713@gmail.com>
2024-11-03 23:33:20 +0900

    feat(Programmers): P12906

---
+++ src/programmers/level_0/P12906.java
@@ -0,0 +1,28 @@
+package programmers.level_0;
+
+import java.util.*;
+
+
+public class P12906 {
+        public static int[] solution(int []arr) {
+            Stack<Integer> stack = new Stack<>();
+            stack.push(arr[0]);
+
+            for (int i : arr) {
+                int priorIndex = stack.peek();
+                if (priorIndex != i) stack.push(i);
+            }
+
+            int[] answer = new int[stack.size()];
+
+            for(int i = answer.length -1; i >= 0; i--) {
+                answer[i] = stack.pop();
+            }
+            return answer;
+        }
+
+    public static void main(String[] args) {
+        int[] arr = {1,1,3,3,0,1,1};
+        System.out.println(Arrays.toString(solution(arr)));
+    }
+}
