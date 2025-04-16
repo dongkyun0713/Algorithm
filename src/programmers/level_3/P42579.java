f5f1c0394fe7a30b30164a55ce4ef2f914f343d6
dongkyunKim <kyun010713@gmail.com>
2025-04-16 22:28:30 +0900

    programmers: P42579

---
+++ src/programmers/level_3/P42579.java
@@ -0,0 +1,11 @@
+package programmers.level_3;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class P42579 {
+    public int[] solution(String[] genres, int[] plays) {
+        Map<String, Integer> map = new HashMap<>();
+        return null;
+    }
+}
