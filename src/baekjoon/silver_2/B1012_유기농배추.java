985c096291990908880db936bc791c4d98e9ee52
dongkyunKim <kyun010713@gmail.com>
2025-10-23 17:22:41 +0900

    baekjoon: B1012

--- src/baekjoon/silver_2/B1012_유기농배추.java
+++ src/baekjoon/silver_2/B1012_유기농배추.java
@@ -1,9 +1,57 @@
 package baekjoon.silver_2;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class B1012_유기농배추 {
-    public static void main(String[] args) throws IOException {
+    static int[][] grid;
 
+    private static void dfs(int i, int j, int[][] grid) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 0) {
+            return;
+        }
+        grid[i][j] = 0;
+        dfs(i, j + 1, grid);
+        dfs(i, j - 1, grid);
+        dfs(i - 1, j, grid);
+        dfs(i + 1, j, grid);
+    }
+
+    private static int numIsWorm(int[][] grid) {
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    dfs(i, j, grid);
+                    ++count;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int M = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken());
+            int K = Integer.parseInt(st.nextToken());
+            grid = new int[N][M];
+            for (int j = 0; j < K; j++) {
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                grid[y][x] = 1;
+            }
+
+            sb.append(numIsWorm(grid)).append("\n");
+        }
+        System.out.println(sb);
     }
 }
