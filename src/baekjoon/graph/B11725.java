8cc6f17fa8ae78f207746a0477f559ff6a7a4565
dongkyunKim <kyun010713@gmail.com>
2026-01-12 18:18:21 +0900

    B11725

---
+++ src/baekjoon/graph/B11725.java
@@ -0,0 +1,12 @@
+package baekjoon.graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B11725 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
