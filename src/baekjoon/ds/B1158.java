571314487d454822282f68cbe108140f13c4cd52
dongkyunKim <kyun010713@gmail.com>
2026-01-26 16:19:15 +0900

    B1158

---
+++ src/baekjoon/ds/B1158.java
@@ -0,0 +1,35 @@
+package baekjoon.ds;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class B1158 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        Queue<Integer> q = new ArrayDeque<>();
+        for (int i = 1; i <= N; i++) {
+            q.add(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        while (q.size() != 1) {
+            for (int i = 0; i < K - 1; i++) {
+                q.add(q.remove());
+            }
+            sb.append(q.remove()).append(", ");
+
+        }
+        sb.append(q.remove()).append(">");
+
+        System.out.println(sb);
+    }
+}
