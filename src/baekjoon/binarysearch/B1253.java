22db1bd9d37160ed0e7f7b6722f867f315be8b9e
dongkyunKim <kyun010713@gmail.com>
2026-02-18 18:01:50 +0900

    B1253

---
+++ src/baekjoon/binarysearch/B1253.java
@@ -0,0 +1,54 @@
+package baekjoon.binarysearch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class B1253 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        int answer = 0;
+        for (int i = 0; i < N; i++) {
+            int left = 0;
+            int right = N - 1;
+            boolean isGood = false;
+            int target = arr[i];
+
+            while (left < right) {
+                if (left == i) {
+                    left++;
+                    continue;
+                }
+                if (right == i) {
+                    right--;
+                    continue;
+                }
+                int sum = arr[left] + arr[right];
+
+                if (sum == target) {
+                    isGood = true;
+                    break;
+                } else if (sum > target) {
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+            if (isGood) {
+                answer++;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
