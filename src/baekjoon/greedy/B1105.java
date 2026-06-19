f28eb1a017688a6433f76aa7e9c0342615066757
dongkyunKim <kyun010713@gmail.com>
2026-06-19 14:20:39 +0900

    B1105

---
+++ src/baekjoon/greedy/B1105.java
@@ -0,0 +1,34 @@
+package baekjoon.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B1105 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String L = st.nextToken();
+        String R = st.nextToken();
+
+        if (L.length() != R.length()) {
+            System.out.println(0);
+            return;
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < L.length(); i++) {
+            if (L.charAt(i) != R.charAt(i)) {
+                break;
+            }
+
+            if (L.charAt(i) == '8') {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
